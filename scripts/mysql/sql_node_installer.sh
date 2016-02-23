0a4f1e64add214cc8dbd3e199b7827340a6c2e1c
Jianshen Liu <jliu120@ucsc.edu>
2016-02-22 23:50:49 -0800

    Upadte sql_node configuration file

--- scripts/mysql/sql_node_installer.sh
+++ scripts/mysql/sql_node_installer.sh
@@ -6,12 +6,50 @@ if [ $# -eq 0 ]
     exit 0
 fi
 
-PACKAGE=mysql-cluster-gpl-7.4.10-linux-glibc2.5-x86_64.tar.gz
-DESC=/var/tmp
 SCRIPT=$0
 CONTAINER_ID=$1
 
-echo "CONTAINER_ID=${CONTAINER_ID}"
+echo "CONTAINER_ID is ${CONTAINER_ID}"
+
+# Deploy my.cnf
+# Automatically find the container id of management node (name=mgm).
+echo -e "\nDeploying my.cnf..."
+
+# grep -w : match whole word
+mgmId=$(docker ps | grep -w mgm | awk '{print $1}')
+
+if [ -z "$mgmId" ]; then
+    echo "Cannot find management node with name=mgm. Please start management node first."
+    exit 0
+fi
+
+echo "Container ID of Management Node is ${mgmId}"
+
+MGM_IP=$(docker inspect ${mgmId} | grep \"IPAddress\" | awk 'NR==1{print $2}' | tr -d \",)
+
+echo "IPAddress of Management Node is ${MGM_IP}"
+
+# See http://www.cyberciti.biz/faq/unix-linux-appleosx-bsd-bash-script-find-what-directory-itsstoredin/
+SCRIPT_LOCATION=$(dirname $(readlink -f ${SCRIPT}))
+
+MY_CNF_TEMPLATE=${SCRIPT_LOCATION}/my_data_sql_node.cnf
+
+content=$(cat ${MY_CNF_TEMPLATE})
+content=$(sed -e "s@#MGM_IP@${MGM_IP}@g" <<< "$content")
+
+MY_CNF_PATH_TMP=${SCRIPT_LOCATION}/my.cnf.tmp
+
+echo "${content}" > ${MY_CNF_PATH_TMP}
+
+docker cp ${MY_CNF_PATH_TMP} ${CONTAINER_ID}:/etc/my.cnf
+
+rm ${MY_CNF_PATH_TMP}
+
+# Install MySQL Server
+echo -e "\nInstalling MySQL Server..."
+
+PACKAGE=mysql-cluster-gpl-7.4.10-linux-glibc2.5-x86_64.tar.gz
+DESC=/var/tmp
 
 echo "Copying resources..."
 
@@ -19,9 +57,6 @@ docker cp "${HOME}/softwares/${PACKAGE}" "${CONTAINER_ID}:${DESC}"
 
 # :2:3 Remove the first 2 chars and the last 3 chars
 REMOTE_SCRIPT="${SCRIPT:2:-3}-remote.sh"
-
-# See http://www.cyberciti.biz/faq/unix-linux-appleosx-bsd-bash-script-find-what-directory-itsstoredin/
-SCRIPT_LOCATION=$(dirname $(readlink -f ${SCRIPT}))
 
 REMOTE_SCRIPT_PATH=${SCRIPT_LOCATION}/${REMOTE_SCRIPT}
 
@@ -36,4 +71,5 @@ docker exec ${CONTAINER_ID} chmod +x ${DESC}/${REMOTE_SCRIPT}
 
 rm ${REMOTE_SCRIPT_PATH}.tmp
 
-echo "Please execute script ${REMOTE_SCRIPT} in container ${CONTAINER_ID}."
+echo -e "\nDone!"
+echo "Please execute script ${DESC}/${REMOTE_SCRIPT} in container ${CONTAINER_ID}."
